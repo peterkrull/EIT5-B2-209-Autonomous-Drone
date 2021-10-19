1a50561e1c5584e51527a218b75ba67a0408bd90
Peter Krull <peterkrullpeter@gmail.com>
2021-10-19 20:48:39 +0200

    Converted to class and added getTimeStampedData function

--- dev-tools/raspberry_socketreader.py
+++ dev-tools/raspberry_socketreader.py
@@ -1,24 +1,42 @@
 import socket
+import time
 import struct as s
 
-# Raspberry PI - IP and port
-rcv_UDP_IP = "0.0.0.0"
-rcv_UDP_PORT = 51001
+class viconUDP:
 
-# Websocket setup
-in_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-in_sock.bind((rcv_UDP_IP, rcv_UDP_PORT))
+    # Raspberry PI - IP and VICON port
+    def __init__(self,ip = "0.0.0.0",port = 51001):
+        self.time_start = time.time()
 
-def getPosRot():
-        data, addr = in_sock.recvfrom(4096) # buffer size is 1024 bytes
+        # Websocket setup
+        self.in_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.in_sock.bind((ip, port))
 
-        pos = []
-        for i in range(3): pos.append( s.unpack_from('d', data, 32+i*8)[0])
+    def getPosRot(self):
+        data, addr = self.in_sock.recvfrom(4096) # buffer size is 1024 bytes
 
-        rot = []
-        for i in range(3): rot.append( s.unpack_from('d', data, 56+i*8)[0])
+        pos = [s.unpack_from('d', data, 32+i*8)[0] for i in range(3)]
+        rot = [s.unpack_from('d', data, 56+i*8)[0] for i in range(3)]
 
         return pos,rot
 
-while True:
-        print(getPosRot() )
+    def getTimestampedData(self,unix_time = False):
+        """
+        Get Vicon data with timestamp
+
+        Args:
+            unix_time (bool) : Save timestamp as raw unix time instead of time since object creation
+
+        return ( list[float] ) : timestamp, xpos, ypos, zpos, xrot, yrot, zrot
+        """
+        data, addr = self.in_sock.recvfrom(4096) # buffer size is 1024 bytes
+
+        xdata = [s.unpack_from('d', data, 32+i*8)[0] for i in range(6)]
+        if unix_time:   xdata.insert(0,time.time())
+        else:           xdata.insert(0,time.time()-self.time_start)
+        return xdata
+
+if __name__ == '__main__':
+    vicon = viconUDP()
+    while True:
+        print(vicon.getTimestampedData())
