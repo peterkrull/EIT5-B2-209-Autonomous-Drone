536bc7eb786199beab1d870e87c8a7a98f9a65da
Peter Krull <peterkrullpeter@gmail.com>
2021-10-20 00:26:25 +0200

    Update drone_main.py

--- dev-tools/drone_main.py
+++ dev-tools/drone_main.py
@@ -17,10 +17,25 @@ if __name__ == '__main__':
 
     PID = [pid_thrust,pid_pitch,pid_roll,pid_yaw]
 
+    setpoint = [-1000,1000,1000]
+
+    # setup crazyFlie client
     cf = easyflie()
+    cf.send_start_setpoint()
 
     try:
         while True:
+            vicon_data = vicon_udp.getTimestampedData() # fetch vicon data
+            vicon_log.log_data(vicon_data) # save data with timestamp
+            vicon_data.pop(0) # remove timestamp from dataset
+
+            # Calculate error in position
+            x_error = setpoint[0]-vicon_data[0]
+            y_error = setpoint[1]-vicon_data[1]
+            y_error = setpoint[2]-vicon_data[2]
+
+            
+
             # MAIN PROGRAM LOOP
             pass
     except KeyboardInterrupt:
