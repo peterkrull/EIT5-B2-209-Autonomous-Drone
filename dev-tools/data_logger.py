d66a1b7715c2f06e76eb16919c340dedc6d91aac
Peter Krull <peterkrullpeter@gmail.com>
2021-10-19 21:58:42 +0200

    logger file_name is defined at object creation

--- dev-tools/data_logger.py
+++ dev-tools/data_logger.py
@@ -2,14 +2,15 @@ import csv
 import time
 
 class logger:
-    def __init__(self):
+    def __init__(self,file_name):
+        self.file_name = file_name
         self.start_time = time.time()
         self.data = []
 
     def log_data(self,data):
         self.data.append(data)
 
-    def save_file(self,file_name = "data_exported"):
-        with open(str(int(self.start_time))+"_"+file_name+".csv",'w',newline='',encoding='UTF8') as file:
+    def save_file(self):
+        with open(str(int(self.start_time))+"_"+self.file_name+".csv",'w',newline='',encoding='UTF8') as file:
             writer = csv.writer(file)
             writer.writerows(self.data)
