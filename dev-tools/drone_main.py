9d0467c06cd2c587bb6e4179440124c5c5bedea5
Peter Krull <peterkrullpeter@gmail.com>
2021-10-22 11:53:23 +0200

    Updated drone_main.py for initial flight tests

--- dev-tools/drone_main.py
+++ dev-tools/drone_main.py
@@ -8,40 +8,36 @@ import time
 
 # Constantly load setpoint from file
 def thread_setpoint_loader():
-    global constant_setpoint,running
-    constant_setpoint = {"x":0,"y":0,"z":0}
+    global setpoint,running
     while running:
-        try: constant_setpoint = json.load(open("dev-tools/const_setpoint.json"))
-        except FileNotFoundError: constant_setpoint = json.load(open("const_setpoint.json"))
+        try: setpoint = json.load(open("dev-tools/const_setpoint.json"))
+        except FileNotFoundError: setpoint = json.load(open("const_setpoint.json"))
         time.sleep(0.5)
 
 # Main program / control loop
 def thread_main_loop():
-    global constant_setpoint,running
+    global setpoint,running
     while running:
         # Get vicon data and log it
-        #vicon_data = vicon_udp.getTimestampedData() # fetch vicon data
-        vicon_data = ["time",123,456,789,100,200,300]
+        vicon_data = vicon_udp.getTimestampedData() # fetch vicon data
         vicon_log.log_data(vicon_data) # save data with timestamp
         vicon_data.pop(0) # remove timestamp from dataset
 
         # Calculate error in position
-        #x_error = setpoint[0]-vicon_data[0]
-        #y_error = setpoint[1]-vicon_data[1]
-        #z_error = setpoint[2]-vicon_data[2]
-        z_error = constant_setpoint.get('z') #- vicon_data[2]
-
+        x_error = setpoint.get('x')-vicon_data[0]
+        y_error = setpoint.get('y')-vicon_data[1]
+        z_error = setpoint.get('z')-vicon_data[2]
+        
         # Get updated control from PID
         thrust = pid_thrust.update(z_error) + hover_thrust
-        #pitch = pid_pitch.update(x_error)
-        #roll = pid_roll.update(y_error)
-        #yaw = pid_yaw.update(None) #?
+        pitch = pid_pitch.update(x_error)
+        roll = pid_roll.update(y_error)
+        yaw = pid_yaw.update(None) #?
 
         # Send updated control params
-        #cf.send_setpoint(roll,pitch,yaw,thrust)
-        time.sleep(0.1)
+        cf.send_setpoint(roll,pitch,yaw,thrust)
+        time.sleep(0.005) # wait for 5 ms, or 1/200Hz (allows other threads to run)
         
-
 if __name__ == '__main__':
 
     # constans
@@ -53,18 +49,13 @@ if __name__ == '__main__':
     # Setup vicon udp reader and logger
     vicon_udp = viconUDP()
     vicon_log = logger("vicon_log")
-    vicon_log.log_data(["time","xpos","ypos","zpos","xrot","yrot","zrot"])
-
-    # Initially all setpoints are constant
-    setpoint = [-1000,1000,1000]
-
+     
     # setup crazyFlie client
     cf = easyflie()
-    #cf.send_start_setpoint()
-    pitch, roll, yaw, thrust = 0,0,0,0
+    cf.send_start_setpoint()
 
     # Setup PID control for all axes
-    pid_thrust = control.PID(Kp=10)
+    pid_thrust = control.PID(Kp=5000)
     pid_pitch = control.PID(0)
     pid_roll = control.PID(0)
     pid_yaw = control.PID(0)
@@ -73,23 +64,22 @@ if __name__ == '__main__':
     for pid in PID:
         pid.start()
 
+    # Tells treads to keep running
     running = True
 
+    # Start program threads
     loader = Thread(target=thread_setpoint_loader)
     loader.start()
     time.sleep(0.2)
-
     main = Thread(target=thread_main_loop)
     main.start()
 
-    
-
+    # Handle program exit correctly
     while 1 :
-        try:
-            time.sleep(0.1)
+        try: time.sleep(0.2)
         except KeyboardInterrupt:
             vicon_log.save_file()
             print("1")
-            #cf.send_stop_setpoint()
+            cf.send_stop_setpoint()
             running = False
-            exit("what")
+            exit("Exiting program")
