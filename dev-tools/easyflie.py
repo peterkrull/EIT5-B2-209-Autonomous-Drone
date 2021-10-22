2883acf0baa59e3b9ac357ff87f98282dc3a0b7d
Peter Krull <peterkrullpeter@gmail.com>
2021-10-22 11:14:10 +0200

    easyflie.py now has default URI

--- dev-tools/easyflie.py
+++ dev-tools/easyflie.py
@@ -1,14 +1,13 @@
-import time
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 
 class easyflie:
 
-    def __init__(self,URI):
-        self.URI = 'radio://0/80/2M'
+    def __init__(self,URI = None):
+        self.URI = 'radio://0/80/2M' if not URI else URI
         cflib.crtp.init_drivers()
         self.cf = Crazyflie()
-        
+
     def send_start_setpoint(self):
         self.cf.open_link(self.URI)
         self.cf.commander.send_setpoint(0,0,0,0)
