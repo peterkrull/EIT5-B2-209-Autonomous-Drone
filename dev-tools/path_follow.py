649dba9b7eafcd73f180933d2d0800250f622180
LauLauridsen <lau.lauridsen@hotmail.com>
2021-11-01 12:29:16 +0100

    Update path_follow.py

--- dev-tools/path_follow.py
+++ dev-tools/path_follow.py
@@ -2,6 +2,15 @@ import numpy as np
 
 class PathFollow:
     def __init__(self, radiusUpdate, pathFileLocation) -> None:
+        """
+        Making path object
+
+        Args:
+            radius update (int)     : Radius to point before heading to next point
+            patFileLocation (str)   : Location of path file
+        """
+
+
         self.radiusUpdate = radiusUpdate    #Distance where next ref is taken
         self.path = 0                       #The Path. 
         self.count = 0                      #The step the drone heading for in at in path
@@ -19,9 +28,13 @@ class PathFollow:
         return path
 
     def getRef(self, position):  # Returning cordinate heading for point. Opdates when close to point
-        #Position format x,y,z, yaw. unit: mm
-        # !!!! SO FAR YAW = 0!!!!!
-      
+        """
+        Returns refrence positions, including yaw
+
+        Args:
+            position (int) (x,y,z) : unit mm
+        """
+
         # Beregn afstand mellem position of ref punkt.
         distPosToRef = np.sqrt((self.path[self.count][1] - position[1])**2 + (
             self.path[self.count][2] - position[2])**2 + (self.path[self.count][3] - position[3])**2)
@@ -35,5 +48,5 @@ class PathFollow:
         # return xRef, yRef, zRef, Yaw
 
 #For Testing
-#pathFollow = PathFollow(500,"C:\\Users\\laula\\Documents\\GitHub\\EIT5-B2-209-Autonomous-Drone\\simulations\\pathFollowing\\pathToFollow.csv") 
-#print(pathFollow.getRef([5, 5, 5, 5]))
+pathFollow = PathFollow(500,"C:\\Users\\laula\\Documents\\GitHub\\EIT5-B2-209-Autonomous-Drone\\simulations\\pathFollowing\\pathToFollow.csv") 
+print(pathFollow.getRef([5, 5, 5, 5]))
