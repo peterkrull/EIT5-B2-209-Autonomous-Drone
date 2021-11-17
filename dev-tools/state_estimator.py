4a75e7962d909e460e3cda7ac9de1a2d2bcdbadc
soerenLang <58945910+soerenLang@users.noreply.github.com>
2021-11-17 14:18:50 +0100

    Update state_estimator.py

--- dev-tools/state_estimator.py
+++ dev-tools/state_estimator.py
@@ -3,22 +3,22 @@ from pitchRoll_estimator import pitchRoll_estimator
 
 class state_estimator:
     def __init__(self, init_pos):
-        self.est_pos = {'x','y','z'}
+        self.est_pos = {'x','y','z','yaw'}
         self.xy_estimator = pitchRoll_estimator(init_pos)
         self.pos = {'x','y','z','yaw'}
+
 
     def update(self, vicon_data, drone_data):
         gyro_data = {'x' : drone_data['gyro_x'], 'y' : drone_data['gyro_y'], 'z' : drone_data['gyro_z']}
         acc_data = {'x': drone_data['acc_x'], 'y': drone_data['acc_y'], 'z': drone_data['acc_z']}
 
-
+        #state estimation missing z
+        self.est_pos['yaw'] = drone_data['stateEstimate_yaw']
+        self.est_pos['x','y'] = self.xy_estimator.update(gyro_data, acc_data,self.self.pos['yaw'])
+        self.est_pos['z'] = vicon_data['z'] #update pos using z-estimator
+        
         if vicon_data['vicon_availabe'] == 1:
-            #state estimation missing z
-            self.pos['yaw'] = drone_data['stateEstimate_yaw']
-            self.pos['x','y'] = self.xy_estimator.update(gyro_data, acc_data,self.yaw)
-            self.pos['z'] = vicon_data['z'] #update pos using 
+            #use vicon to acquire actual position to improve positioning of data.
+            pass
+            
 
-            # update reference position based on vicon
-        else:
-            #state estimation missing z
-            self.xy_estimator.update(self.xy_estimator.update(gyro_data, acc_data,self.yaw))
