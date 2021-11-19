a1c45c982ebfb30f9f0e117a825bb44139371bec
soerenLang <58945910+soerenLang@users.noreply.github.com>
2021-11-19 11:58:18 +0100

    Update soerensComplimentary.m

--- test-results/onboard-sensor-drift/soerensComplimentary.m
+++ test-results/onboard-sensor-drift/soerensComplimentary.m
@@ -1,14 +1,37 @@
-data = readmatrix('1636983493_sensorDrift');
+data = readmatrix('1637063677_test_flyvning');
+
 
 data(:,1) = data(:,1)-data(1,1);
-prevAngle = 0;
+prevAngle = [0 0];
 timeDif = data(2,1)-data(1,1);
-a = .3;
+a = .33;
 
 for i = 1 :length(data)
-    estAngle(i) = a*atan(data(i,6)/(data(i,7))) +(1-a)*(prevAngle + data(i,3)*timeDif); 
-    prevAngle = estAngle(i);
+    if(~isnan(data(i,30)))
+        estAngle(i,1) = a*atan(data(i,29)/(data(i,31))) +(1-a)*(prevAngle(1) + data(i,26)*timeDif); 
+        prevAngle(1) = estAngle(i,1);
+        
+        estAngle(i,2) = a*atan(data(i,30)/(data(i,31))) +(1-a)*(prevAngle(2) + data(i,27)*timeDif); 
+        prevAngle(2) = estAngle(i,2);
+    end
 end
 
 estAngle = estAngle*180/pi;
-plot(data(:,1),estAngle)
+x_axis = linspace(data(1,1), data(length(data),1),length(estAngle)); 
+figure(1)
+plot(x_axis,estAngle(:,1))
+hold on 
+plot(data(:,1), data(:,5)*180/pi)
+hold off 
+grid on 
+title('Pitch angle')
+legend('Estimated pitch','Measured pitch')
+
+figure(2)
+plot(x_axis,estAngle(:,2))
+hold on 
+plot(data(:,1), data(:,6)*180/pi)
+hold off 
+grid on 
+title('Roll angle')
+legend('Estimated roll','Measured roll')
