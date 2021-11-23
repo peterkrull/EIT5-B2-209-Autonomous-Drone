1b0e29af20d5f24232a3fb6822a8d0f6d3101230
soerenLang <58945910+soerenLang@users.noreply.github.com>
2021-11-23 12:34:16 +0100

    Added independent k-factors for pitch and roll

--- dev-tools/state_estimator.py
+++ dev-tools/state_estimator.py
@@ -2,7 +2,7 @@ from pitchRoll_estimator import pitchRoll_estimator
 from ComplimentaryThrust import thrust_estimator
 
 class state_estimator:
-    def __init__(self, init_pos, vicon_udp, Kx = .34, Kz = 0.9):
+    def __init__(self, init_pos, vicon_udp, Kx = .34, Ky = .34, Kz = 0.9):
         """
         Initiates the state estimator based on an initial state
 
@@ -13,7 +13,7 @@ class state_estimator:
 
         self.init_pos = init_pos
         self.est_pos = init_pos
-        self.xy_estimator = pitchRoll_estimator(init_pos, vicon_udp, Kx)
+        self.xy_estimator = pitchRoll_estimator(init_pos, vicon_udp, Kx, Ky)
         self.z_estimator = thrust_estimator(Kz,30)
         self.pos = init_pos
 
